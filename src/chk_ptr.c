9f9c6d9b70d67727622fcd3ea83d0764ea024dc4
root <root@localhost.localdomain>
2019-08-14 07:58:58 -0400

    insert in binary

---
+++ src/chk_ptr.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   chk_ptr.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmaury <fmaury@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/12 14:16:29 by fmaury            #+#    #+#             */
+/*   Updated: 2019/05/03 13:17:01 by fmaury           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "nm.h"
+
+int				string_oef(char *str, t_wdy *file)
+{
+	size_t	i;
+
+	i = 0;
+	if (!chk_ptr(file, (void*)(str), sizeof(char)))
+		return (0);
+	while (str[i])
+	{
+		i++;
+		if (!chk_ptr(file, (void*)(str + i), sizeof(char)))
+		{
+			str[i - 1] = '\0';
+			return (1);
+		}
+	}
+	return (1);
+}
+
+static int		bfr_file(void *bgn_file, void *bgn_ptr)
+{
+	if (bgn_file > bgn_ptr)
+		return (0);
+	return (1);
+}
+
+static int		aft_file(void *end_file, void *end_ptr)
+{
+	if (end_file < end_ptr)
+		return (0);
+	return (1);
+}
+
+int				chk_ptr(t_wdy *file, void *begin, size_t size)
+{
+	if (!bfr_file(file->begin, begin))
+		return (0);
+	if (!aft_file(file->ptr + file->size, begin + size))
+		return (0);
+	return (1);
+}
--- src/insert_pack.c
+++ src/insert_pack.c
@@ -1,49 +1,77 @@
 #include <woody.h>
 
-void find_offset(t_wdy *obj)
+int find_offset(t_wdy *obj)
 {
+    int i = 0;
+    
     Elf64_Ehdr *hdr;
     Elf64_Shdr *shdr;
-    int i = 0;
-    Elf64_Shdr *sec_name;
+    Elf64_Shdr *secname_section;
+    char *secname;
 
     hdr = (Elf64_Ehdr*)obj->ptr;
     obj->entry = hdr->e_entry;
-    printf("entry:%ld\n",hdr->e_shoff);
+    obj->entry_addr = &hdr->e_entry;
+    if (!chk_ptr(wdy, obj->ptr, hdr->e_shoff))
+    {
+        ft_putstr("Pointer error\n");
+        return (0);
+    }
     shdr = obj->ptr + hdr->e_shoff;
-    sec_name = shdr + hdr->e_shstrndx;
+    if (!chk_ptr(wdy, shdr, hdr->e_shstrndx))
+    {
+        ft_putstr("Pointer error\n");
+        return (0);
+    }
+    secname_section = shdr + hdr->e_shstrndx;
     while (i < hdr->e_shnum)
     {
-    printf("name:%s\n", (char*)(obj->ptr + sec_name->sh_offset + shdr->sh_name));
+        if (!chk_ptr(wdy, obj->ptr, secname_section->sh_offset + shdr->sh_name) ||
+        !chk_ptr(wdy, shdr, sizeof(*shdr)))
+        {
+            ft_putstr("Pointer error\n");
+            return (0);
+        }
+        secname = (char*)(obj->ptr + secname_section->sh_offset + shdr->sh_name);
+        printf("name:%s\n", secname);
+        if (!strcmp(secname, ".plt"))
+            return ((int)shdr->sh_offset);
         shdr++;
         i++;
     }
+    return 0;
     
 }
 
 int check_null_space(t_wdy *obj)
 {
-    // size_t     i;
-    // size_t   nbyte;
-    // char       *tmp_ptr;
+    size_t         i;
+    size_t         nbyte;
+    char       *tmp_ptr;
 
-    // i = 0;
-    find_offset(obj);
-    // nbyte = i;
-    // tmp_ptr = (char *)obj->ptr;
-    // while (i < obj->size)
-    // {
-    //     if (tmp_ptr[i])
-    //         nbyte = i + 1;
-    //     else if (i - nbyte >= SHELLCODE_LEN)
-    //         return (nbyte);
-    //     i++;
-    // }
-    return (-1);
+    i = find_offset(obj);
+    if (!i)
+    {
+        ft_putstr("Can't write in binary's sections\n");
+        return (-1);
+    }
+    nbyte = i;
+    tmp_ptr = (char *)obj->ptr;
+    while (i < obj->size)
+    {
+        if (tmp_ptr[i])
+            nbyte = i + 1;
+        else if (i - nbyte >= SHELLCODE_LEN)
+            return (nbyte);
+        i++;
+    }
+    ft_putstr("Not enough NULL bytes to write in this binary\n");
+    return (-2);
 }
 
 void insert_shellcode(t_wdy *obj, int offset)
 {
+    *(uint64_t *)obj->entry_addr = offset;
     ft_memcpy(obj->ptr + offset, ELF64_SHELLCODE, SHELLCODE_LEN);
 }
 
@@ -55,8 +83,13 @@ int insert_pack(t_wdy *obj)
     if ((offset = check_null_space(obj)) == -1)
         return (0);
     insert_shellcode(obj, offset);
-    fp = open("packed", O_CREAT | O_WRONLY, 0777);
+    if (!(fp = open("packed", O_CREAT | O_WRONLY, 0777) == -1 ))
+    {
+        printf("Can't open %s\n", packed);
+        return (0);
+    }
     write(fp, obj->ptr, obj->size);
-    close(fp);
+    if (close(fp) == -1)
+        return (0);
     return (1);
 }
